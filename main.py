c767c6333453457360c615a08fed07c7110eaa61
Kevin Chandra Wijaya <59856557+kvnchndr@users.noreply.github.com>
2023-04-04 11:09:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -48,6 +48,9 @@ def get_ps(df,i):
         (ps,ts) = (',','.') if len(df.loc[i].split('.')[1])>=3 else ('.',',') 
     elif df.loc[i].find(',')!=-1 :
         (ps,ts) = ('.',',') if len(df.loc[i].split(',')[1])>=3 else (',','.')
+    else:
+        ps = "."
+        ts = ","
     return df.loc[i].replace(ps,'koma').replace(ts,',').replace(',','').replace('koma','.')
 
 
@@ -62,7 +65,7 @@ a = ",".join(df['official_store'])
 official_store = st.selectbox('Please choose the official Store',a.split(','))
 
 
-uploaded_file = st.file_uploader(label='')
+uploaded_file = st.file_uploader(label='Upload Input Files',label_visibility='collapsed')
 if uploaded_file is None :
     st.error("Upload a .csv file first: [Input Data.csv](https://docs.google.com/spreadsheets/d/1eNKYbBJ-FKBM-y4QDu4BiyqnywqgXIFRujABblVqWXc/edit#gid=0)")
 else :
@@ -116,7 +119,7 @@ else :
                     if df['rrp_incl_vat'].dtypes == 'object':
                         df['rrp_incl_vat']=df['rrp_incl_vat'].astype(str).str.lower().replace(string_to_replace_dict,regex=True)
                         for i in range (len(df)):
-                            df['rrp_incl_vat'].loc[i] = get_ps(df['rrp_incl_vat'],i)
+                            df.loc[i,'rrp_incl_vat'] = get_ps(df['rrp_incl_vat'],i)
                         df['rrp_incl_vat'] = df['rrp_incl_vat'].astype(float)
                     else :
                         df['rrp_incl_vat'] = df['rrp_incl_vat'].astype(float)
@@ -132,7 +135,7 @@ else :
                         if df['promo_discount_percent'].dtypes == 'object':
                             df['promo_discount_percent']=df['promo_discount_percent'].astype(str).str.lower().replace(string_to_replace_dict,regex=True)
                             for i in range (len(df)):
-                                df['promo_discount_percent'].loc[i] = get_ps(df['promo_discount_percent'],i)
+                                df.loc[i,'promo_discount_percent'] = get_ps(df['promo_discount_percent'],i)
                             df['promo_discount_percent'] = df['promo_discount_percent'].astype(float)
                         else :
                             df['promo_discount_percent'] = df['promo_discount_percent'].astype(float)
@@ -148,7 +151,7 @@ else :
                             if df['promo_discount_amount'].dtypes == 'object':
                                 df['promo_discount_amount']=df['promo_discount_amount'].astype(str).str.lower().replace(string_to_replace_dict,regex=True)
                                 for i in range (len(df)):
-                                    df['promo_discount_amount'].loc[i] = get_ps(df['promo_discount_amount'],i)
+                                    df.loc[i,'promo_discount_amount'] = get_ps(df['promo_discount_amount'],i)
                                 df['promo_discount_amount'] = df['promo_discount_amount'].astype(float)
                             else :
                                 df['promo_discount_amount'] = df['promo_discount_amount'].astype(float)
@@ -164,7 +167,7 @@ else :
                                 if df['rrp_promo_incl_vat'].dtypes == 'object':
                                     df['rrp_promo_incl_vat']=df['rrp_promo_incl_vat'].astype(str).str.lower().replace(string_to_replace_dict,regex=True)
                                     for i in range (len(df)):
-                                        df['rrp_promo_incl_vat'].loc[i] = get_ps(df['rrp_promo_incl_vat'],i)
+                                        df.loc[i,'rrp_promo_incl_vat']  = get_ps(df['rrp_promo_incl_vat'],i)
                                     df['rrp_promo_incl_vat'] = df['rrp_promo_incl_vat'].astype(float)
                                 else :
                                     df['rrp_promo_incl_vat'] = df['rrp_promo_incl_vat'].astype(float)
@@ -180,7 +183,7 @@ else :
                                     if df['rbp_incl_vat'].dtypes == 'object':
                                         df['rbp_incl_vat']=df['rbp_incl_vat'].astype(str).str.lower().replace(string_to_replace_dict,regex=True)
                                         for i in range (len(df)):
-                                            df['rbp_incl_vat'].loc[i] = get_ps(df['rbp_incl_vat'],i)
+                                            df.loc[i,'rbp_incl_vat'] = get_ps(df['rbp_incl_vat'],i)
                                         df['rbp_incl_vat'] = df['rbp_incl_vat'].astype(float)
                                     else :
                                         df['rbp_incl_vat'] = df['rbp_incl_vat'].astype(float)
